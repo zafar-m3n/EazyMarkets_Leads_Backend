4c73e761bd6ec67b15870308b83b1b99374a12ff
zafar-m3n <thezafar.m3n@gmail.com>
2026-08-31 19:06:18 +0530

    Payload Increased

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ connectDB();
 const app = express();
 
 // ✅ Middleware
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(morgan("dev"));
 
 app.use(
